f8e021bf51e409f0300f1eec6e43055e0344e5d0
farazhashmi21 <developerfaraz2yahoo.com>
2021-12-11 21:33:20 +0000

    Continue: Writing your first Django app, part 7.

--- mysite.sh
+++ mysite.sh
@@ -3,4 +3,4 @@ echo $(git init);
 echo $(git add .);
 echo $(git commit -m "Continue: Writing your first Django app, part 7.");
 echo $(git remote -v);
-echo $(git push origin main);
+echo $(git push origin master);
